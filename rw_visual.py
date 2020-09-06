1469fe5f907d0cb27b66f9171dc3d40062356cde
Allen Crump <RACrump3@gmail.com>
2020-09-06 16:39:16 -0400

    Molecular motion. Went back and did 15-3 last instad of first

--- rw_visual.py
+++ rw_visual.py
@@ -6,20 +6,14 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active
 while True:
     # Make a random walk
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(5_000)
     rw.fill_walk()
 
     # Plot the points in the walk
     plt.style.use('classic')
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
-               edgecolor='none', s=1)
-
-    # Emphasize the first and last points
-    ax.scatter(0, 0, c='green', edgecolor='none', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none',
-               s=100)
+    ax.plot(rw.x_values, rw.y_values, linewidth=1)
 
     # Remove the axes
     ax.get_xaxis().set_visible(False)
