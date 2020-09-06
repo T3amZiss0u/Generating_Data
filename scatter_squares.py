aba97536fa3d9f9a92c35c2ff2ac5da3be766880
Allen Crump <RACrump3@gmail.com>
2020-09-06 13:55:55 -0400

    Saving your plots automatically

--- scatter_squares.py
+++ scatter_squares.py
@@ -18,4 +18,4 @@ ax.tick_params(axis='both', which='major', labelsize=14)
 # Set range for each axis
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
