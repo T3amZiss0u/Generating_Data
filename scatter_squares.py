8aebb4d3529c91feaef718eb719fe4cbe10a44c6
Allen Crump <RACrump3@gmail.com>
2020-09-06 13:54:09 -0400

    Using a Colormap

--- scatter_squares.py
+++ scatter_squares.py
@@ -5,7 +5,7 @@ y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn')
 fig, ax = plt .subplots()
-ax.scatter(x_values, y_values, c=(0, 0.8, 0),  s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues,  s=10)
 
 # Set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -16,6 +16,6 @@ ax.set_ylabel("Square of Value", fontsize=14)
 ax.tick_params(axis='both', which='major', labelsize=14)
 
 # Set range for each axis
-ax.axis([0,1100, 0, 1100000])
+ax.axis([0, 1100, 0, 1100000])
 
 plt.show()
